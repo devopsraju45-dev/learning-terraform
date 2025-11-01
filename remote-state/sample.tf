6826c32383e9a6bdcc88fbeadfa9124ff108996b
prudhviratnakaram <devopsraju45@gmail.com>
2025-11-01 12:51:15 -0500

    Add componenet scripts

---
+++ remote-state/sample.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "terraformstste"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "aws" {
+  ami = "ami-005d1c2f7f99cdb06"
+  instance_type = "t3.micro"
+}
