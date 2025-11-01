39fd848314da8b40cf1be50184de06b0a169ce46
prudhviratnakaram <devopsraju45@gmail.com>
2025-11-01 13:53:24 -0500

    Add componenet scripts

--- modules/sample2/ec2/test.tf
+++ modules/sample2/ec2/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "aws" {
   ami = "ami-005d1c2f7f99cdb06"
-  instance_type = "t3.micro"
-  tags = {
-    Name = "MyWebServer1"
-  }
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [var.sgid]
 }
+
+variable "sgid" {}
