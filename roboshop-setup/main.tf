0b9303898cda89d558b3b0a25617f5e0145c982f
prudhviratnakaram <devopsraju45@gmail.com>
2025-11-04 19:54:58 -0600

    Add componenet scripts

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -16,3 +16,25 @@ resource "aws_route53_record" "record" {
   ttl     = 30
   records = [aws_instance.app.*.private_ip[count.index]]
 }
+
+resource "null_resource" "ansible-apply" {
+  depends_on = [aws_route53_record.record]
+  triggers = {
+    abc = timestamp()
+  }
+  count = length(var.components)
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.app.*.public_ip[count.index]
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "sudo labauto clean",
+      "ansible-pull -i localhost, -U https://github.com/devopsraju45-dev/roboshop-mutable-ansible roboshop.yml -e HOSTS=localhost -e APP_COMPONENT_ROLE=${var.components[count.index]} -e ENV=dev"
+    ]
+
+  }
+}
