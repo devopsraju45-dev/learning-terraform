1664bda51104d5006218bde5a643a40e5da4134f
prudhviratnakaram <devopsraju45@gmail.com>
2025-11-04 19:58:16 -0600

    Add componenet scripts

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "app" {
   count                  = length(var.components)
   ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
+  iam_instance_profile   = "secretmanager"
   tags = {
     Name = "${var.components["${count.index}"]}-dev"
   }
